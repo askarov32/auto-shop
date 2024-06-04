0890ac19dbea78a44963b53148707bf285ffedf2
askarov32 <insar@lessons.kz>
2024-06-04 19:15:22 +0500

    Created methods getMoreCars and getCars in CarService

--- src/main/java/spring/boot/auto_shop/service/CarService.java
+++ src/main/java/spring/boot/auto_shop/service/CarService.java
@@ -1,10 +1,13 @@
 package spring.boot.auto_shop.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import spring.boot.auto_shop.models.Car;
 import spring.boot.auto_shop.repository.CarRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,4 +23,16 @@ public class CarService {
     public List<Car> getAllCars() {
         return carRepository.findAll();
     }
+
+    public List<Car> getMoreCars(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        return carRepository.findAll(pageable).getContent();
+    }
+
+    public List<Car> getCars(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        return carRepository.findAll(pageable).getContent();
+    }
+
+
 }
