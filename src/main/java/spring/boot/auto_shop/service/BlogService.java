f117ac22dd5bb124f4d0f483565e08e2a22eded3
askarov32 <insar@lessons.kz>
2024-06-11 14:35:33 +0500

    Created method that finds blog by id

--- src/main/java/spring/boot/auto_shop/service/BlogService.java
+++ src/main/java/spring/boot/auto_shop/service/BlogService.java
@@ -3,16 +3,20 @@ package spring.boot.auto_shop.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.boot.auto_shop.models.News;
-import spring.boot.auto_shop.repository.NewsRepository;
+import spring.boot.auto_shop.repository.BlogRepository;
 
 import java.util.List;
 
 @Service
-public class NewsService {
+public class BlogService {
     @Autowired
-    private NewsRepository newsRepository;
+    private BlogRepository blogRepository;
 
     public List<News> getAllNews() {
-        return newsRepository.findAll();
+        return blogRepository.findAll();
+    }
+
+    public News getBlogById(int id) {
+        return blogRepository.findById(id).orElseThrow();
     }
 }
