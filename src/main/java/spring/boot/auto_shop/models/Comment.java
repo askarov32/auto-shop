ad562347f8cbf103aa62dfdb90ffff78bc9a17af
askarov32 <insar@lessons.kz>
2024-06-13 14:18:06 +0500

    Created Comment model

---
+++ src/main/java/spring/boot/auto_shop/models/Comment.java
@@ -0,0 +1,29 @@
+package spring.boot.auto_shop.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id")
+    private Blog blog;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
