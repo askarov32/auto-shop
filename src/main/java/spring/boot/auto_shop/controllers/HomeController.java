63e935da95965d65f3de477cb6dc1b09d64c47d5
askarov32 <insar@lessons.kz>
2024-06-22 14:07:13 +0500

    Created sign in page

--- src/main/java/spring/boot/auto_shop/controllers/HomeController.java
+++ src/main/java/spring/boot/auto_shop/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package spring.boot.auto_shop.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,28 +15,31 @@ import spring.boot.auto_shop.service.CarService;
 import java.util.List;
 
 @Controller
-public class CarController {
+public class HomeController {
 
     private final CarService carService;
     private final BlogService blogService;
 
     @Autowired
-    public CarController(CarService carService, BlogService blogService) {
+    public HomeController(CarService carService, BlogService blogService) {
         this.carService = carService;
         this.blogService = blogService;
     }
 
+
+    @PreAuthorize("isAuthenticated()")
+    @Transactional
     @GetMapping("/")
-    public String getIndexPage(Model model) {
-        List<Car> cars = carService.getCars(1, 8);
-        List<Blog> blogs = blogService.getAllBlogs();
-        for (Blog blog1 : blogs) {
-            if (blog1.getContent().length() > 255) {
-                blog1.setContent(blog1.getContent().substring(0, 255) + "...");
-            }
-        }
-        model.addAttribute("cars", cars);
-        model.addAttribute("blogList", blogs);
+    public String getIndexPage() {
+//        List<Car> cars = carService.getCars(1, 8);
+//        List<Blog> blogs = blogService.getAllBlogs();
+//        for (Blog blog1 : blogs) {
+//            if (blog1.getContent().length() > 255) {
+//                blog1.setContent(blog1.getContent().substring(0, 255) + "...");
+//            }
+//        }
+//        model.addAttribute("cars", cars);
+//        model.addAttribute("blogList", blogs);
         return "index";
     }
 
@@ -50,19 +55,23 @@ public class CarController {
         return "blog";
     }
 
-
-    @GetMapping("/blog-details")
-    public String blogDetails(@RequestParam("id") int id, Model model) {
+    @GetMapping("/blog-info")
+    public String blogInfo(@RequestParam("id") int id, Model model) {
         Blog blog = blogService.getBlogById(id);
         model.addAttribute("blog", blog);
-        return "blog-details";
+        return "blog-info";
     }
 
+    @GetMapping("blog-details")
+    public String blogDetails() {
+        return "blog-details";
+    }
     @GetMapping("/about")
     public String aboutPage() {
         return "about";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/car-details")
     public String carDetailsPage() {
         return "car-details";
@@ -89,13 +98,20 @@ public class CarController {
     }
 
     @GetMapping("/show-blogs")
-    public  String listBlogs(Model model) {
+    public String listBlogs(Model model) {
         model.addAttribute("blogListFr", blogService.getAllBlogs());
         return "fragments/blog :: blogListFragment";
     }
-
-    @GetMapping("/login")
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/sign-in")
     public String login() {
-        return "login";
+        return "sign-in";
     }
+
+//    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
 }
